c1e3f09d498882301aa859a85d3a042caea5b0c1
bootloopmaster636 <christhartono@hotmail.com>
2023-08-20 11:26:52 +0700

    add items json

--- lib/classes/item.dart
+++ lib/classes/item.dart
@@ -1,14 +1,16 @@
 class item {
   late String nama;
   late String jenis;
+  late int harga;
   late int jumlah;
   String? catatan;
 
-  item({required this.nama, required this.jenis, required this.jumlah, this.catatan});
+  item({required this.nama, required this.jenis,required this.harga, required this.jumlah, this.catatan});
 
   item.fromJson(Map<String, dynamic> json) {
     nama = json['nama'];
     jenis = json['jenis'];
+    harga = json['harga'];
     jumlah = json['jumlah'];
     catatan = json['catatan'];
   }
@@ -17,6 +19,7 @@ class item {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['nama'] = this.nama;
     data['jenis'] = this.jenis;
+    data['harga'] = this.harga;
     data['jumlah'] = this.jumlah;
     data['catatan'] = this.catatan;
     return data;
