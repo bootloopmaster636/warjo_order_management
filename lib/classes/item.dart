c91018a9740f629a8d454a8a3a8fc5c1456246e3
bootloopmaster636 <christhartono@hotmail.com>
2023-08-22 10:07:11 +0700

    Move menu data to the class itself instead of json, so we don't have an additional file

--- lib/classes/item.dart
+++ lib/classes/item.dart
@@ -5,23 +5,54 @@ class item {
   late int jumlah;
   String? catatan;
 
-  item({required this.nama, required this.jenis,required this.harga, required this.jumlah, this.catatan});
+  item(
+      {required this.nama, required this.jenis, required this.harga, required this.jumlah, this.catatan});
 
-  item.fromJson(Map<String, dynamic> json) {
-    nama = json['nama'];
-    jenis = json['jenis'];
-    harga = json['harga'];
-    jumlah = json['jumlah'];
-    catatan = json['catatan'];
+  static List<item> makeMenuMakanan() {
+    final List<item> menu = [
+      //menu utama
+      item(nama: "Bubur Ayam", jenis: "Makanan", harga: 8000, jumlah: 0),
+      item(nama: "Nasi Soto Ayam", jenis: "Makanan", harga: 8000, jumlah: 0),
+      item(nama: "Nasi Timlo", jenis: "Makanan", harga: 10000, jumlah: 0),
+      item(nama: "Nasi Bakmoy Ayam", jenis: "Makanan", harga: 10000, jumlah: 0),
+
+      //menu utama upsize
+      item(nama: "Nasi Soto Ayam UPSIZE", jenis: "Makanan", harga: 12000, jumlah: 0),
+      item(nama: "Nasi Timlo UPSIZE", jenis: "Makanan", harga: 14000, jumlah: 0),
+      item(nama: "Nasi Bakmoy Ayam UPSIZE", jenis: "Makanan", harga: 14000, jumlah: 0),
+
+      //tambahan
+      item(nama: "Nasi Putih", jenis:"Makanan", harga: 4000, jumlah: 0),
+      item(nama: "Telor Kecap", jenis:"Makanan", harga: 4000, jumlah: 0),
+
+      //bungkus
+      item(nama: "(BUNGKUS) Bubur Ayam", jenis: "Makanan", harga: 10000, jumlah: 0),
+      item(nama: "(BUNGKUS) Nasi Soto Ayam", jenis: "Makanan", harga: 10000, jumlah: 0),
+      item(nama: "(BUNGKUS) Nasi Timlo", jenis: "Makanan", harga: 12000, jumlah: 0),
+      item(nama: "(BUNGKUS) Nasi Bakmoy Ayam", jenis: "Makanan", harga: 12000, jumlah: 0),
+    ];
+    return menu;
   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['nama'] = this.nama;
-    data['jenis'] = this.jenis;
-    data['harga'] = this.harga;
-    data['jumlah'] = this.jumlah;
-    data['catatan'] = this.catatan;
-    return data;
+  static List<item> makeMenuMinuman() {
+    final List<item> menu = [
+      //minuman
+      item(nama: "Teh Manis", jenis: "Minuman", harga: 3000, jumlah: 0),
+      item(nama: "Teh Tawar", jenis: "Minuman", harga: 3000, jumlah: 0),
+      item(nama: "Teh Mondo", jenis: "Minuman", harga: 3000, jumlah: 0),
+      item(nama: "Es Teh Manis", jenis: "Minuman", harga: 3000, jumlah: 0),
+      item(nama: "Es Teh Tawar", jenis: "Minuman", harga: 3000, jumlah: 0),
+      item(nama: "Es Teh Mondo", jenis: "Minuman", harga: 3000, jumlah: 0),
+      item(nama: "Teh Kampul", jenis: "Minuman", harga: 4000, jumlah: 0),
+      item(nama: "Es Teh Kampul", jenis: "Minuman", harga: 4000, jumlah: 0),
+      item(nama: "Jeruk Peras Hangat", jenis: "Minuman", harga: 4000, jumlah: 0),
+      item(nama: "Es Jeruk Peras", jenis: "Minuman", harga: 4000, jumlah: 0),
+      item(nama: "Kopi", jenis: "Minuman", harga: 5000, jumlah: 0),
+      item(nama: "Es Kopi", jenis: "Minuman", harga: 5000, jumlah: 0),
+      item(nama: "Air Hangat", jenis: "Minuman", harga: 1000, jumlah: 0),
+      item(nama: "Air Es", jenis: "Minuman", harga: 1000, jumlah: 0),
+      item(nama: "Air Mineral Botol", jenis: "Minuman", harga: 4000, jumlah: 0),
+    ];
+    return menu;
   }
 }
